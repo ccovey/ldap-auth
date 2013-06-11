95a998eccdc0bcb6ccfb4cf7867684ee7bf5ca7f
Cody Covey <ccovey14@gmail.com>
2013-06-11 16:21:16 -0500

    before update

--- src/LDAPAuth/LDAPAuthServiceProvider.php
+++ src/LDAPAuth/LDAPAuthServiceProvider.php
@@ -5,23 +5,22 @@
  *
  * @author ccovey
  */
+
 namespace LDAPAuth;
 
 use Illuminate\Auth as Auth;
 
-class LDAPAuthServiceProvider extends Auth\AuthServiceProvider{
-    public function register()
-	{
-		$this->app['auth'] = $this->app->share(function($app)
-		{
-			// Once the authentication service has actually been requested by the developer
-			// we will set a variable in the application indicating such. This helps us
-			// know that we need to set any queued cookies in the after event later.
-			$app['auth.loaded'] = true;
+class LDAPAuthServiceProvider extends Auth\AuthServiceProvider {
 
-			return new LDAPAuthManager($app);
-		});
-	}
+    public function register() {
+        $this->app['auth'] = $this->app->share(function($app) {
+                    // Once the authentication service has actually been requested by the developer
+                    // we will set a variable in the application indicating such. This helps us
+                    // know that we need to set any queued cookies in the after event later.
+                    $app['auth.loaded'] = true;
+
+                    return new LDAPAuthManager($app);
+                });
+    }
+
 }
-
-?>
